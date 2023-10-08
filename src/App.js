98247ab9a355d8e951c6395f9b973aaa235906af
PatelAbhi1108 <pjitendra124@gmail.com>
2023-10-08 18:30:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,59 +1,58 @@
 import React from "react";
-import Navbar from  "./components/Navbar";
-import Cards from "./components/Cards"
-import Filter from "./components/Filter"
-import { apiUrl, filterData  } from "./data";
-import { useState,useEffect } from "react";
+import Navbar from "./components/Navbar";
+import Cards from "./components/Cards";
+import Filter from "./components/Filter";
+import { apiUrl, filterData } from "./data";
+import { useState, useEffect } from "react";
 import Spinner from "./components/Spinner";
-import {toast} from "react-toastify";
+import { toast } from "react-toastify";
 
-
-const App = () => { 
+const App = () => {
   const [courses, setCourses] = useState(null);
   const [loading, setLoading] = useState(true);
   const [category, setCategory] = useState(filterData[0].title);
 
   async function fetchData() {
     setLoading(true);
-    try{
+    try {
       let response = await fetch(apiUrl);
       let output = await response.json();
-      ///output -> 
+      ///output ->
       setCourses(output.data);
-    }
-    catch(error) {
-        toast.error("Network me koi dikkat hai");
+    } catch (error) {
+      toast.error("Network me koi dikkat hai");
     }
     setLoading(false);
   }
 
   useEffect(() => {
     fetchData();
-  }, [])
-  
+  }, []);
 
   return (
     <div className="min-h-screen flex flex-col bg-bgDark2">
       <div>
-        <Navbar/>
+        <Navbar />
       </div>
       <div className="bg-bgDark2">
         <div>
-          <Filter 
-          filterData={filterData}
+          <Filter
+            filterData={filterData}
             category={category}
             setCategory={setCategory}
           />
         </div>
-        <div className="w-11/12 max-w-[1200px] 
-        mx-auto flex flex-wrap justify-center items-center min-h-[50vh]">
-        {
-            loading ? (<Spinner/>) : (<Cards courses={courses} category={category}/>)
-          }
+        <div
+          className="w-11/12 max-w-[1200px] 
+        mx-auto flex flex-wrap justify-center items-center min-h-[50vh]"
+        >
+          {loading ? (
+            <Spinner />
+          ) : (
+            <Cards courses={courses} category={category} />
+          )}
         </div>
       </div>
-
-
     </div>
   );
 };
